a67bf2c5e2b35262e5f2e2e2b9fbb04b91342ebf
Lauren Abrams <lauren.abrams@powerhrg.com>
2018-08-02 10:07:08 -0400

    one more commit before deleting devise

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
+  devise_for :users, :controllers => { registrations: 'registrations' }
   resources :vets
   resources :animals
-  devise_for :users
   resources :users do
     resources :animals
     resources :appointments

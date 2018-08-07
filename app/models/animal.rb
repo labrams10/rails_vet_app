fc89840a487330565ce925b1d0aeb0f7c375336a
Lauren Abrams <lauren.abrams@powerhrg.com>
2018-08-07 15:16:40 -0400

    made slight changes to the animal index page

--- app/models/animal.rb
+++ app/models/animal.rb
@@ -3,12 +3,15 @@
 class Animal < ActiveRecord::Base
   belongs_to :user
   validates :name, :weight, :breed, :age, presence: true
-  scope :users_animals, -> { where(user: user) }
+
+  def self.users_animals(user)
+    where(user: user)
+  end
 
   # NOTE: this is a different way to execute the
   # same functionality around using scope
 
-  # def self.users_animals(user)
-  #   where(user: user)
-  # end
+  # scope :users_animals, -> { where(user: user) }
+
+
 end
